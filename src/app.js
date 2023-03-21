2fa2d056073b0ca6a4cb4a91aef4ca4267264ebe
CristopherAreche <cristopherareche764@gmail.com>
2023-03-20 20:01:51 -0400

    test 3

--- src/app.js
+++ src/app.js
@@ -12,29 +12,16 @@ const server = express();
 
 server.name = "API";
 
-// const whitelist = [
-//   "https://pokemon-backend-6ohr.onrender.com/",
-//   "https://cristopherareche.github.io/pokemon-website/",
-//   "http://localhost:3000",
-// ];
-server.use(
-  cors()
-  //   {
-  //   origin: (origin, cb) => {
-  //     if (whitelist.includes(origin)) {
-  //       cb(null, true);
-  //     } else {
-  //       cb(new Error("Not allowed by CORS"));
-  //     }
-  //   },
-  // }
-);
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://pokemon-backend-6ohr.onrender.com"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
